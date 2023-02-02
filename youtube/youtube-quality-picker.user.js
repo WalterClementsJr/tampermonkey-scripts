2d8517d610c46cb5c71d23807e1db812af4f50ab
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-02-02 23:16:06 +0700

    Fix youtube quality picker

--- youtube/youtube-quality-picker.user.js
+++ youtube/youtube-quality-picker.user.js
@@ -7,7 +7,7 @@
 // @downloadURL  https://github.com/WalterClementsJr/tampermonkey-scripts/blob/main/youtube/youtube-quality-picker.user.js
 // @updateURL    https://github.com/WalterClementsJr/tampermonkey-scripts/blob/main/youtube/youtube-quality-picker.user.js
 // @grant        GM_addStyle
-// @match        https://www.youtube.com/watch?*
+// @match        https://www.youtube.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @run-at       document-start
 // ==/UserScript==
@@ -122,7 +122,12 @@
     }
 
     let css = ".button-12 { display: flex; flex-direction: column; align-items: center; padding: 6px 14px; font-family: -apple-system, BlinkMacSystemFont, 'Roboto', sans-serif; border-radius: 6px; border: none; background: #0f0f0f; box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1), inset 0px 0.5px 0.5px rgba(255, 255, 255, 0.5), 0px 0px 0px 0.5px rgba(0, 0, 0, 0.12); color: #DFDEDF; user-select: none; -webkit-user-select: none; touch-action: manipulation; } .button-12:focus { box-shadow: inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2), 0px 0.5px 1px rgba(0, 0, 0, 0.1), 0px 0px 0px 3.5px rgba(58, 108, 217, 0.5); outline: 0; }"
-    createButtons();
-
     GM_addStyle(css);
+
+    window.addEventListener('yt-page-data-updated', function () {
+        console.log('page data reloaded');
+        if (window.location.href !== "https://www.youtube.com/") {
+            createButtons();
+        }
+    });
 })();
