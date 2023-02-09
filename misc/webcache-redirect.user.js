31ce3347b87beda1fa5a0f334a075653c33f1034
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2023-02-09 10:59:20 +0700

    add webcache script

---
+++ misc/webcache-redirect.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         WebCache redirector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  redirect all urls to webcache
+// @author       WalterClementsJr
+// @match        https://webcache.googleusercontent.com/search*
+// @grant        none
+// @downloadURL  https://github.com/WalterClementsJr/tampermonkey-scripts/raw/main/misc/webcache-redirect.user.js
+// @updateURL    https://github.com/WalterClementsJr/tampermonkey-scripts/raw/main/misc/webcache-redirect.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const anchors = document.getElementsByTagName("a");
+    const redirect = "https://webcache.googleusercontent.com/search?q=cache:";
+
+    for (let i = 0; i < anchors.length; i++) {
+        anchors[i].href = redirect + anchors[i].href;
+    }
+})();
