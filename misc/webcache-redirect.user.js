bb8cde0e895a94b6d2c1116988b68166abe04fc7
Walter <61227405+WalterClementsJr@users.noreply.github.com>
2023-03-14 16:05:40 +0700

    update links generation

--- misc/webcache-redirect.user.js
+++ misc/webcache-redirect.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WebCache redirector
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  redirect all urls to webcache
 // @author       WalterClementsJr
 // @match        https://webcache.googleusercontent.com/search*
@@ -13,8 +13,8 @@
 (function() {
     'use strict';
 
-    const body = document.querySelector("body > div:last-child");
-    const anchors = body.getElementsByTagName("a");
+    const doc = [...document.querySelectorAll("body > div")].pop();
+    const anchors = doc.getElementsByTagName("a");
     const redirect = "https://webcache.googleusercontent.com/search?q=cache:";
 
     for (let i = 0; i < anchors.length; i++) {
