00b2572695ae0d76a3bd9e2fe21af51880cd6076
Walter <61227405+WalterClementsJr@users.noreply.github.com>
2023-06-11 16:09:20 +0700

    Update youtube-quality-picker.user.js

--- youtube/youtube-quality-picker.user.js
+++ youtube/youtube-quality-picker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Video Quality Picker
 // @namespace    https://github.com/WalterClementsJr
-// @version      0.1.2
+// @version      0.1.3
 // @description  add buttons to select video quality in 2 clicks less
 // @author       walterwalker
 // @downloadURL  https://github.com/WalterClementsJr/tampermonkey-scripts/blob/main/youtube/youtube-quality-picker.user.js
@@ -74,7 +74,7 @@
 
         if (!selection) {
             let qualityTexts = qualityOptions.map(el => el.innerText).join('\n');
-            console.log('"' + quality + '" not found. Options are: \n\nHighest\n' + qualityTexts);
+            // close the setting wheel
             settingsButton.click();
             return;
         }
@@ -104,7 +104,11 @@
         waitForElm(titleCssSelector).then((elm) => {
             console.log('Element is ready');
 
-            document.querySelector(titleCssSelector).appendChild(btnLocation);
+            const container = document
+            .querySelector(titleCssSelector)
+            .parentElement;
+            container.appendChild(btnLocation);
+            container.style.cssText += 'display: flex; flex-direction: column;';
 
             // create buttons
             for (let i of qualities) {
@@ -121,7 +125,7 @@
         });
     }
 
-    let css = ".button-12 { display: flex; flex-direction: column; align-items: center; padding: 6px 14px; font-family: -apple-system, BlinkMacSystemFont, 'Roboto', sans-serif; border-radius: 6px; border: none; background: #0f0f0f; box-shadow: 0px 0.5px 1px rgba(0, 0, 0, 0.1), inset 0px 0.5px 0.5px rgba(255, 255, 255, 0.5), 0px 0px 0px 0.5px rgba(0, 0, 0, 0.12); color: #DFDEDF; user-select: none; -webkit-user-select: none; touch-action: manipulation; } .button-12:focus { box-shadow: inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2), 0px 0.5px 1px rgba(0, 0, 0, 0.1), 0px 0px 0px 3.5px rgba(58, 108, 217, 0.5); outline: 0; }"
+    let css = ".button-12{display:flex;flex-direction:column;align-items:center;padding:6px 14px;font-family:-apple-system,BlinkMacSystemFont,Roboto,sans-serif;border-radius:6px;border:none;background:#0f0f0f;box-shadow:0 .5px 1px rgba(0,0,0,.1),inset 0 .5px .5px rgba(255,255,255,.5),0 0 0 .5px rgba(0,0,0,.12);color:#fff}";
     GM_addStyle(css);
 
     window.addEventListener('yt-navigate-finish', function () {
